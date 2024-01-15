4cad52c66c47c89b9dabfa84ecc01720dbf8823c
Andrew <andbushuev@yandex.ru>
2024-01-15 11:59:58 +0500

    Initial commit 15.01.24

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 # main/urls.py
 from django.urls import path
 from . import views
-from .views import BlogPostCreateView, BlogPostDetailView, BlogPostListView
+from .views import BlogPostCreateView, BlogPostDetailView, BlogPostListView, CreateProductView, UpdateProductView, \
+    DeleteProductView, ContactView
+from user_app.views import DashboardView, UserLoginView, RegisterView
 
 # Определение маршрутов для приложения 'main'
 app_name = 'main'
@@ -16,9 +18,12 @@ urlpatterns = [
 
     # Страница контактов
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
-    path('contacts/', views.contact_view, name='contacts'),
 
     # Страница детального представления продукта
+    path('contacts/', ContactView.as_view(), name='contacts'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
     # product_id передается в качестве аргумента
     path('product/<int:product_id>/', views.ProductDetailView.as_view(), name='product_detail'),
 
@@ -26,9 +31,9 @@ urlpatterns = [
     path('blog/<slug:slug>/', BlogPostDetailView.as_view(), name='post_detail'),
     path('blog/', views.BlogPostListView.as_view(), name='post_list'),
     path('blog/', BlogPostListView.as_view(), name='blogpost_list'),
-    path('create/', views.create_product, name='create_product'),
+    path('create/', CreateProductView.as_view(), name='create_product'),
     path('list/', views.product_list, name='product_list'),
-    path('update/<int:pk>/', views.update_product, name='update_product'),
-    path('delete/<int:pk>/', views.delete_product, name='delete_product'),
+    path('update/<int:pk>/', UpdateProductView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', DeleteProductView.as_view(), name='delete_product'),
 
 ]
--- user_app/urls.py
+++ user_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RegisterView, UserLoginView, DashboardView
 
 urlpatterns = [
-    path('register/', RegisterView, name='register'),  # URL для регистрации пользователя
-    path('login/', UserLoginView, name='login'),  # URL для входа пользователя
-    path('dashboard/', DashboardView, name='dashboard'),  # Защищенный URL для просмотра информации пользователя
+    path('register/', RegisterView.as_view(), name='register'),  # URL для регистрации пользователя
+    path('login/', UserLoginView.as_view(), name='login'),  # URL для входа пользователя
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),  # Защищенный URL для просмотра информации пользователя
 ]
